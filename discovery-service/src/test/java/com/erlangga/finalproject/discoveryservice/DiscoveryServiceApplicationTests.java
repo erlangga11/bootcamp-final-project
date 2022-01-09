b76eb084e09106ba653fde55f4b4fe4e8f2d8055
Erlangga Natanegara Putra Aryando <erlangganpa@gmail.com>
2022-01-10 03:22:52 +0700

    Complete service

---
+++ discovery-service/src/test/java/com/erlangga/finalproject/discoveryservice/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erlangga.finalproject.discoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
